c87be34c168481750a2d3c3c778681031df3a9c8
Hubert <9610397@gmail.com>
2024-02-13 16:51:53 +0800

    update script

--- train_script.py
+++ train_script.py
@@ -14,16 +14,18 @@ def get_parser():
     parser.add_argument("--batch_size", type=str, help="batch size", default=32)
     parser.add_argument("--epoches", type=str, help="epoches", default=750)
     parser.add_argument("--model_name", type=str, help="model name", default="lstm_market")
-    parser.add_argument("--save_model", type=bool, help="if save model == false, then save checkpoint", default=True)
+    parser.add_argument("--save_model", type=bool, help="if save model == false, then save checkpoint", default=False)
     parser.add_argument("--checkpoint_name", type=str, help="checkpoint name", default="checkpoint")
     parser.add_argument("--use_early_stopping", type=str, help="use early stopping", default=True)
     parser.add_argument("--learning_rate", type=str, help="learning rate", default=0.00001)
     parser.add_argument("--model_type", type=str, help="model type", default="LSTM")
     parser.add_argument("--patience", type=str, help="patience", default=250)
     parser.add_argument("--min_delta", type=str, help="min delta", default=0.001)
+    parser.add_argument("--targets", nargs='+', help="targets", default=['close', 'high', 'low'])
     return parser
 
 def main(args):
+    # init args
     ticker = args.ticker
     time_window_size = args.time_window_size
     batch_size = args.batch_size
@@ -36,20 +38,23 @@ def main(args):
     model_type = args.model_type
     patience = args.patience
     min_delta = args.min_delta
+    targets = args.targets
 
+    # check data file exists
     if not os.path.exists(f'./datas/{ticker}.csv'):
         raise Exception(f"file {ticker}.csv not found")
 
     data = pd.read_csv(f'./datas/{ticker}.csv')
 
+    # create indicators and scale datasets
     data_processed = create_indicators(data=data)
     data_scaled_np, scalers = scale_datasets(data=data_processed)
 
-    targets = ['close', 'high', 'low']
     y_idx_list = get_y_idx(data_processed.columns, targets)
 
     train_np, test_np = split_datasets(data_scaled_np, 0.6)
 
+    # Train class需要使用到的資料
     original_data = {
         'datas': test_np[time_window_size:],
         'scalers': scalers,
@@ -58,6 +63,7 @@ def main(args):
         'plot': targets
     }
 
+    # model參數
     model_params = {
         'input_size': len(data_processed.columns),
         'output_size': len(targets),
@@ -68,6 +74,7 @@ def main(args):
         'batch_first': True
     }
 
+    # convert to lstm format
     train_data_loader = convert_to_lstm_format(train_np, time_window_size, y_idx_list, batch_size=batch_size)
     test_x_data, test_y_data = convert_to_lstm_format(test_np, time_window_size, y_idx_list)
 

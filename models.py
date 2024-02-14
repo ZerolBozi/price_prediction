1b6aacb1098def0155180e0dacdf32c3df1979f0
Hubert <9610397@gmail.com>
2024-02-14 15:43:36 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,23 @@
 import torch
 import torch.nn as nn
 
+"""
+時間序列 參數說明:
+    input_size: 輸入的特徵數量
+    output_size: 輸出的特徵數量
+    hidden_size: 隱藏層的cell數量
+    fc_size: 全連接層的cell數量
+    num_layers: 模型的層數
+    dropout_prob: dropout的比例, 防止過擬合, 訓練過程中丟掉一些cell
+    batch_first: 輸入的格式, True: (batch, seq, feature), False: (seq, batch, feature),
+                    因為convert_to_lstm_format函數的輸出是(batch, seq, feature), 所以這邊設為True
+
+深度強化學習 參數說明:
+    input_size: 輸入的特徵數量
+    n_actions: 輸出的動作數量
+    units: 全連接層的cell數量
+"""
+
 class Model:
     def __init__(self, model_path:str='./models/'):
         self.model_path = model_path
@@ -12,9 +29,9 @@ class Model:
         return model
     
 class DQN(nn.Module):
-    def __init__(self, input_shape:int, n_actions:int, units: int=32):
+    def __init__(self, input_size:int, n_actions:int, units: int=32):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(input_shape, units)
+        self.fc1 = nn.Linear(input_size, units)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(units, n_actions)
 
